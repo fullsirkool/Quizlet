c4d2365e28941e3799b521fddd699d7ce02e0104
Trần Việt Huy <tvhtranviethuy1@gmail.com>
2022-01-17 23:42:30 +0700

    aaa
    aaaa

--- quizlet/src/App.vue
+++ quizlet/src/App.vue
@@ -20,8 +20,10 @@
   font-family: "Prompt";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
-  // color: #2c3e50;
+}
+* {
+  margin: 0px;
+  padding: 0px;
 }
 
 #nav {
